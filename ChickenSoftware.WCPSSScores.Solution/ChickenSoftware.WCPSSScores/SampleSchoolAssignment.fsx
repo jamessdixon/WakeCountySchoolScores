2b30ac9240358927dcf789c8f5265971cf76c1ee
Jamie Dixon <jamessdixon@gmail.com>
2015-02-26 20:04:25 -0500

    Added new files

---
+++ ChickenSoftware.WCPSSScores.Solution/ChickenSoftware.WCPSSScores/SampleSchoolAssignment.fsx
@@ -0,0 +1,71 @@
+﻿#r "../packages/FSharp.Data.2.1.1/lib/net40/FSharp.Data.dll"
+#r "../packages/Microsoft.Azure.Documents.Client.0.9.2-preview/lib/net40/Microsoft.Azure.Documents.Client.dll"
+#r "../packages/Newtonsoft.Json.4.5.11/lib/net40/Newtonsoft.Json.dll"
+#r "C:/Program Files (x86)/Reference Assemblies/Microsoft/Framework/.NETFramework/v4.5.1/System.Net.Http.dll"
+
+open System
+open System.IO
+open FSharp.Data
+open System.Linq
+open Newtonsoft.Json
+open System.Net.Http
+open Microsoft.Azure.Documents
+open System.Text.RegularExpressions
+open Microsoft.Azure.Documents.Linq
+open Microsoft.Azure.Documents.Client
+
+type HouseAssignment = JsonProvider<"../data/HouseAssignmentSample.json">
+
+let getSchools (index:int) =
+    try
+        let endpointUrl = "https://chickensoftware.documents.azure.com:443/"
+        let authKey = "rk3sqMc6W/hB6SQEoaL8Yi1dvSn4C5VmvnrMdPSBQna3L8eCLMwnZeIJNpH8graTfV+GRxR2pYUUBFo5rdQuww=="
+        let client = new DocumentClient(new Uri(endpointUrl), authKey) 
+        let database = client.CreateDatabaseQuery().Where(fun db -> db.Id = "wakecounty" ).ToArray().FirstOrDefault()
+        let collection = client.CreateDocumentCollectionQuery(database.CollectionsLink).Where(fun dc -> dc.Id = "houseassignment").ToArray().FirstOrDefault()
+        let documentLink = collection.SelfLink
+        let queryString = "SELECT * FROM houseassignment WHERE houseassignment.houseIndex = " + index.ToString()
+        let query = client.CreateDocumentQuery(documentLink,queryString)
+        match Seq.length query with
+        | 0 -> None                    
+        | _ -> 
+                let firstValue = query |> Seq.head
+                let assignment = HouseAssignment.Parse(firstValue.ToString())
+                Some assignment.Schools
+    with
+        | :? HttpRequestException as ex ->
+            None
+
+//http://stackoverflow.com/questions/6062191/f-getting-a-list-of-random-numbers
+type System.Random with
+    member this.GetValues(minValue, maxValue) =
+        Seq.initInfinite (fun _ -> this.Next(minValue, maxValue))
+
+let random = new System.Random(42)
+let indexes = random.GetValues(1,350000) |> Seq.take(500) |> Seq.toArray
+let allSchools = indexes |> Seq.map(fun i -> getSchools(i)) |> Seq.toArray
+allSchools |> Seq.length
+
+let allSchools' = allSchools |> Seq.filter(fun s -> s.IsSome)
+//allSchools' |> Seq.length
+let allSchools'' = allSchools' |> Seq.collect(fun s -> s.Value)
+//allSchools'' |> Seq.length
+let uniqueSchools = allSchools'' |> Seq.distinct
+uniqueSchools |> Seq.length
+
+
+//allSchools: string[]
+//
+//
+//
+//
+//        let inBase school =
+//            allSchools |> Array.tryFind(fun s -> if s = school then true else false)
+// 
+//        let newSchools = schools |> Array.filter(fun s -> inBase(s).IsNone)
+//        let allSchools' = Array.append allSchools newSchools
+//        getSchools
+//
+//
+//
+//
---
+++ ChickenSoftware.WCPSSScores.Solution/ChickenSoftware.WCPSSScores/SchoolList.fsx
@@ -0,0 +1,31 @@
+﻿
+#r "../packages/FSharp.Data.2.1.1/lib/net40/FSharp.Data.dll"
+
+open System.IO
+open FSharp.Data
+
+type school  = {name:string; calendar:string; address:string; city:string; zipCode:int }
+
+type elementaryContext = HtmlProvider<"../html/elementarySchools.html">
+type middleContext = HtmlProvider<"../html/middleSchools.html">
+type highContext = HtmlProvider<"../html/highSchools.html">
+
+let elementaryRows = elementaryContext.Load("../html/elementarySchools.html").Tables.``Elementary Schools``.Rows
+let middleRows = middleContext.Load("../html/middleSchools.html").Tables.``Middle Schools``.Rows
+let highRows = highContext.Load("../html/highSchools.html").Tables.``High Schools``.Rows
+
+let elemetarySchools = elementaryRows 
+                        |> Seq.map(fun r -> {name=r.Name;calendar=r.Calendar;address=r.Address;city=r.City; zipCode=r.``ZIP Code``})
+                        |> Seq.toArray
+let middleSchools = middleRows 
+                        |> Seq.map(fun r -> {name=r.Name;calendar=r.Calendar;address=r.Address;city=r.City; zipCode=r.``ZIP Code``})
+                        |> Seq.toArray
+let highSchools = highRows 
+                        |> Seq.map(fun r -> {name=r.Name;calendar=r.Calendar;address=r.Address;city=r.City; zipCode=r.``ZIP Code``})
+                        |> Seq.toArray
+
+let schools = Array.append elemetarySchools middleSchools
+let schools' = Array.append schools highSchools
+
+schools' |> Seq.iter(fun s -> printfn "%A" s.name)
+
