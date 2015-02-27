7bbd5a684b89dc133b655ea21966941e04574f97
jamie dixon <jamessdixon@gmail.com>
2015-02-27 15:58:48 +0000

    Added functions

--- ChickenSoftware.WCPSSScores.Solution/ChickenSoftware.WCPSSScores/HousePyramid.fsx
+++ ChickenSoftware.WCPSSScores.Solution/ChickenSoftware.WCPSSScores/HousePyramid.fsx
@@ -12,22 +12,56 @@ open System.Text.RegularExpressions
 open Microsoft.Azure.Documents.Linq
 open Microsoft.Azure.Documents.Client
 
-
 type HouseAssignment = JsonProvider<"../data/HouseAssignmentSample.json">
-type HousePyramid = {index:int; elementarySchool:string; middleSchool:string; highSchool:string}
+type HouseValuation = JsonProvider<"../data/HouseValuationSample.json">
 
 let endpointUrl = "https://chickensoftware.documents.azure.com:443/"
 let authKey = "rk3sqMc6W/hB6SQEoaL8Yi1dvSn4C5VmvnrMdPSBQna3L8eCLMwnZeIJNpH8graTfV+GRxR2pYUUBFo5rdQuww=="
 let client = new DocumentClient(new Uri(endpointUrl), authKey) 
 let database = client.CreateDatabaseQuery().Where(fun db -> db.Id = "wakecounty" ).ToArray().FirstOrDefault()
-let collection = client.CreateDocumentCollectionQuery(database.CollectionsLink).Where(fun dc -> dc.Id = "houseassignment").ToArray().FirstOrDefault()
-let documentLink = collection.SelfLink
-let queryString = "SELECT * FROM houseassignment WHERE houseassignment.houseIndex = 1"
-let query = client.CreateDocumentQuery(documentLink,queryString)
-let firstValue = query |> Seq.head
-let result = HouseAssignment.Parse(firstValue.ToString())
-let housePyramid = {index=result.HouseIndex; elementarySchool=result.Schools.[0];middleSchool=result.Schools.[1];highSchool=result.Schools.[2]}
-let housePyramid' = JsonConvert.SerializeObject(housePyramid)
-File.AppendAllText(@"F:\Git\WakeCountySchoolScores\Data\houseAssignment.json",housePyramid'.ToString() + "," )
+
+let getAssignment (id:int) =
+    let collection = client.CreateDocumentCollectionQuery(database.CollectionsLink).Where(fun dc -> dc.Id = "houseassignment").ToArray().FirstOrDefault()
+    let documentLink = collection.SelfLink
+    let queryString = "SELECT * FROM houseassignment WHERE houseassignment.houseIndex = " + id.ToString()
+    let query = client.CreateDocumentQuery(documentLink,queryString)
+    match query |> Seq.length with
+    | 0 -> None
+    | _ -> 
+        let assignmentValue = query |> Seq.head
+        let assignment = HouseAssignment.Parse(assignmentValue.ToString())
+        Some assignment
+
+let getValuation (id:int) =
+    let collection = client.CreateDocumentCollectionQuery(database.CollectionsLink).Where(fun dc -> dc.Id = "taxinformation").ToArray().FirstOrDefault()
+    let documentLink = collection.SelfLink
+    let queryString = "SELECT * FROM taxinformation WHERE taxinformation.index = 1"
+    let query = client.CreateDocumentQuery(documentLink,queryString)
+    match query |> Seq.length with
+    | 0 -> None
+    | _ -> 
+        let valuationValue = query |> Seq.head
+        let valuation = HouseValuation.Parse(valuationValue.ToString())
+        Some valuation
+
+let assignSchoolTaxBase (id:int) =
+    let assignment = getAssignment(id)
+    let valuation = getValuation(id)
+    match assignment.IsSome,valuation.IsSome with
+    | true, true -> assignment.Value.Schools 
+                    |> Seq.map(fun s -> s, valuation.Value.AssessedValue)
+                    |> Some
+    | _ -> None
+
+
+let indexes = [|1..350000|]
+    
+
+
+
+
+//let housePyramid = {index=result.HouseIndex; elementarySchool=result.Schools.[0];middleSchool=result.Schools.[1];highSchool=result.Schools.[2]}
+//let housePyramid' = JsonConvert.SerializeObject(housePyramid)
+//File.AppendAllText(@"F:\Git\WakeCountySchoolScores\Data\houseAssignment.json",housePyramid'.ToString() + "," )
 
 
